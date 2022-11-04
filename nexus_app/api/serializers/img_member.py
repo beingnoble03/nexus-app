4cc10177117baddb0a61239f37872e177cd1caa5
Noble Mittal <noblemittal@outlook.com>
2022-11-04 13:42:17 +0530

    add extra fields to img member serializer

--- nexus_app/api/serializers/img_member.py
+++ nexus_app/api/serializers/img_member.py
@@ -4,10 +4,10 @@ from nexus_app.models import ImgMember
 
 class ImgMemberSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ["id", "enrolment_number", "name", "username", "year", "email", "image", "is_master"]
+        fields = ["id", "enrolment_number", "name", "username", "year", "email", "image", "is_master", "branch", ]
         model = ImgMember
 
 class ImgMemberNameSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ["id", "name", "is_master"]
+        fields = ["id", "name", "is_master", "image", ]
         model = ImgMember
