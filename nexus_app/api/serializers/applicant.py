8f6cf5d76e3de9d73b8b4647a1d2ca7cb857cc4d
Noble Mittal <noblemittal@outlook.com>
2022-11-04 13:41:17 +0530

    add overridden method for update in applicant serializer

--- nexus_app/api/serializers/applicant.py
+++ nexus_app/api/serializers/applicant.py
@@ -7,3 +7,18 @@ class ApplicantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Applicant
         fields = '__all__'
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if self.initial_data.get("action") == "add":
+            for round in instance.round.all():
+                validated_data["round"].append(round.id)
+        elif self.initial_data.get("action") == "delete":
+            rounds_to_be_removed = validated_data["round"]
+            validated_data["round"] = []
+            for round in instance.round.all():
+                if not round in rounds_to_be_removed:
+                    validated_data["round"].append(round.id)
+        return super().update(instance, validated_data)
