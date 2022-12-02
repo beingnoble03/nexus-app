b6a5a8cd9ab041473d93f9a7286454472fdea218
Noble Mittal <noblemittal@outlook.com>
2022-12-02 14:20:55 +0530

    fix api for previous remarks

--- nexus_app/views/remarks.py
+++ nexus_app/views/remarks.py
@@ -13,48 +13,24 @@ class RemarksView(APIView):
     def get(self, request):
         applicant_id = request.GET.get('applicant_id')
         applicant = Applicant.objects.get(id=applicant_id)
-        print(applicant)
         rounds = applicant.round.all()
-        test_rounds = []
+        questions = []
         for round in rounds:
             if round.round_type == "T":
-                round_dict = {
-                    "id": round.id,
-                    "title": round.round_name,
-                }
-                tests = []
                 for test in round.tests.all():
-                    test_dict = {
-                        "id": test.id,
-                        "title": test.title,
-                    }
-                    sections = []
                     for section in test.section_set.all():
-                        section_dict = {
-                            "id": section.id,
-                            "title": section.title,
-                        }
-                        questions = []
                         for question in section.question_set.all():
                             question_dict = {}
-                            for question_score in question.question_scores.all():
+                            for question_score in question.question_scores.filter(applicant__id = applicant_id):
                                 if question_score.remarks:
                                     question_dict["id"] = question.id
                                     question_dict["title"] = question.title
                                     question_dict["remarks"] = question_score.remarks
                                     question_dict["obtained_marks"] = question_score.obtained_marks
                                     question_dict["maximum_marks"] = question.maximum_marks
+                                    question_dict["test_title"] = test.title
+                                    question_dict["round_name"] = round.round_name
                                     questions.append(question_dict)
-                                    print("he")
-                        if len(questions):
-                            section_dict["questions"] = questions
-                            sections.append(section_dict)
-                    if len(sections):
-                        test_dict["sections"] = sections
-                        tests.append(test_dict)
-                round_dict["tests"] = tests
-                if len(tests):
-                    test_rounds.append(round_dict)
         interviews = applicant.interview_set.all()
         interview_list = []
         for interview in interviews:
@@ -65,6 +41,6 @@ class RemarksView(APIView):
             }
             interview_list.append(interview_dict)
         return Response({
-            "test_rounds": test_rounds,
+            "questions": questions,
             "interviews": interview_list,
         })
